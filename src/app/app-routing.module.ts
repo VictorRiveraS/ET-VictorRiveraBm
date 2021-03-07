cf7e506d79a27402d1003e0cb07f87242a35e479
VictorRiveraS <soma0112@hotmail.com>
2021-03-06 23:19:32 -0600

    version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
-import { NewsSinSesionComponent } from './pages/news-sin-sesion/news-sin-sesion.component'
-import { NewsComponent } from './pages/news/news.component'
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NewsSinSesionComponent } from './pages/news-sin-sesion/news-sin-sesion.component';
+import { NewsComponent } from './pages/news/news.component';
 
 const routes: Routes = [{ path: 'home', component: NewsSinSesionComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'news', component: NewsComponent }]
+  { path: 'news', component: NewsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
